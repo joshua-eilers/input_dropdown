a6c387b5a7a2200f7fff647f9dd97ece443c96ae
Joshua Eilers <eilers.joshua@gmail.com>
2015-05-16 19:17:08 -0700

    basic build script

--- app/src/main.js
+++ app/src/main.js
@@ -1,14 +1,22 @@
-var InputDropdown = require('./input_dropdown.js');
+var InputDropdown = require('./modules/input_dropdown/input_dropdown.js');
 
-$(document).ready(function() {
-	var inputDropdown = new InputDropdown();
+var applicationStart = function() {
 	var items = [];
 
 	for (var i = 0; i < 10; i++) {
 		items.push({ text: "text" + i, val: i });
 	}
 
-	var template = inputDropdown.template('Click me', items);
-	$('#main-content-area').html(template);
-	inputDropdown.init($('#main-content-area > .dropdown'));
-});
+	// var template = inputDropdown.template('Click me', items);
+	// $('#main-content-area').html(template);
+	// inputDropdown.init($('#main-content-area > .dropdown'));
+
+	var inputDropdown = new InputDropdown({
+		buttonName: 'Click me',
+		items: items
+	});
+
+	inputDropdown.appendTo('#main-content-area');
+};
+
+$(document).ready(applicationStart);
--- app/src/modules/input_dropdown/input_dropdown.js
+++ app/src/modules/input_dropdown/input_dropdown.js
@@ -1,80 +1,14 @@
-module.exports = function() {
-	// cache references to primary ui components
-	var ui = {
-		$dropdown: null,
-		$menu: null,
-		$input: null,
-		$items: null
-	};
+var fs = require('fs');
 
-	var api = {};
+module.exports = InputDropdown;
 
-	api.init = function($d) {
-		// cache references to this dropdown and its parts
-		ui.$dropdown = $d;
-		ui.$menu = ui.$dropdown.find('ul.dropdown-menu');
-		ui.$input = ui.$menu.find('li:first > input');
-		ui.$items = ui.$menu.find('.list-group-item');
+function InputDropdown(opts) {
+	var html = fs.readFileSync(__dirname + '/input_dropdown.html', 'utf8');
+	var compiled = _.template(html);
+	this.opts = opts;
+	this.$el = $(compiled({ buttonName: opts.buttonName }));
+}
 
-		// prevent closing the dropdown when clicking the input
-		ui.$input.click(function(event) {
-			event.stopPropagation();
-		});
-
-		// update the button text with the item clicked
-		ui.$items.click(function() {
-			var $a = $(this);
-			ui.$items.removeClass('active');
-			$a.addClass('active');
-			var clickedText = $a.children().eq(0).text();
-			ui.$dropdown.find('.dropdown-btn-text').text(clickedText);
-		});
-
-		// focus the input when showing the dropdown
-		ui.$dropdown.on('shown.bs.dropdown', function() {
-			ui.$input.focus();
-		});
-
-		ui.$input.keyup(function() {
-			var str = $(this).val();
-			if (str.length > 0) {
-				filterList(str);
-			} else {
-				ui.$items.removeClass('match');
-				ui.$items.show();
-			}
-		});
-	};
-
-	// programatically show the dropdown
-	api.show = function() {
-		setTimeout(function() {
-			ui.$dropdown.find('.dropdown-toggle').dropdown('toggle');
-		}, 10);
-	};
-
-	// function to filter the dropdown list based on input
-	var filterList = function(str) {
-		str = str.trim().toLowerCase();
-		ui.$items.filter('.match').removeClass('match');
-
-		ui.$items.filter(function() {
-			return $(this).text().toLowerCase().indexOf(str) >= 0;
-		}).addClass('match').show();
-
-		ui.$items.not('.match').hide();
-	};
-
-	// constructs the markup for a dropdown
-	api.template = function(defaultButtonName, items) {
-		var template = _.template($('#template').html());
-		var data = {
-			defaultButtonName: defaultButtonName,
-			items: items
-		};
-
-		return template(data);
-	};
-
-	return api;
+InputDropdown.prototype.appendTo = function(target) {
+	$(target).append(this.$el);
 };
